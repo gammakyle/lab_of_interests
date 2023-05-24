979ba8fb2b0ff4ad3da9b7197237e2910bf8ffcc
ZiD-Training-Center <degtev_work@mail.ru>
2023-05-24 16:29:07 +0300

    rebuidl doll
    in webgl doll has some artifact, i break one material on 10-12 in chess table, and now this work good. i don't know, maybe it freezy game, but i fix it in future

--- main_menu_scene/scripts/button_scene_main.gd
+++ main_menu_scene/scripts/button_scene_main.gd
@@ -1,7 +1,10 @@
 extends Button
 
+var main_scene = preload("res://fabric_scene/Main.tscn")
+
 func _on_pressed():
-	var main_scene = load("res://main_scene/Main.tscn")
-	var main_scene_instance = main_scene.instance()
-	get_tree().set_current_scene(main_scene_instance)
+	#var main_scene_instance = main_scene.instance()
+	#get_tree().set_current_scene(main_scene_instance)
+	get_tree().change_scene_to_packed(main_scene)
+	
 

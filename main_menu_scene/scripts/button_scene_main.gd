8525b8863be3166a9364bce8563b561b167be7af
Degtev Ivan <degtev_work@mail.ru>
2023-07-08 13:21:50 +0300

    add music, add scene merge, fix dugs

--- main_menu_scene/scripts/button_scene_main.gd
+++ main_menu_scene/scripts/button_scene_main.gd
@@ -1,10 +1,11 @@
 extends Button
 
-var main_scene = preload("res://wood_turner_inside_scene/wood_turner_inside_scene.tscn")
+var main_scene = preload("res://fabric_scene/Main.tscn")
 
 func _on_pressed():
+	var root = get_tree().get_root()
+	var music_object = $"../music_object"
+	root.add_child(music_object)
 	#var main_scene_instance = main_scene.instance()
 	#get_tree().set_current_scene(main_scene_instance)
 	get_tree().change_scene_to_packed(main_scene)
-	
-

823de5db2985926bdd1b22e11abbbc4bd3d75725
Degtev Ivan <degtev_work@mail.ru>
2023-07-17 20:50:41 +0300

    add cnc scene

---
+++ cnc_inside_scene/cnc_input.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	SongScene.GLOBAL_OLD_SCENE = SongScene.GLOBAL_NOW_SCENE
+	SongScene.GLOBAL_NOW_SCENE = 6
+	pass # Replace with function body.

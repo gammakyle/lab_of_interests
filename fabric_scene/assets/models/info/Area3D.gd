cc456a9bc00b42441804b7f6e1357cd5f9b0c60a
ZiD-Training-Center <degtev_work@mail.ru>
2023-07-18 14:45:18 +0300

    some change for textareas

--- fabric_scene/assets/models/info/Area3D.gd
+++ fabric_scene/assets/models/info/Area3D.gd
@@ -1,12 +1,30 @@
 extends Area3D
 
 var local_locker = 0
+@onready var _info_panel: Panel = $"../../../../Player/interface/Panel"
+var alpha = 0.0
+var colorize = false
+var stat = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	
 func _process(delta):
+	if (colorize == true) and (stat == 0):
+		alpha -= delta * 255.0
+		if alpha <= 0.0:
+			alpha = 0.0
+			colorize = false
+			stat = 1
+		_info_panel.modulate = Color(1, 1, 1, alpha/255.0*2)
+	if (colorize == true) and (stat == 2):
+		alpha += delta * 255.0
+		if alpha >= 250.0:
+			alpha = 255.0
+			colorize = false
+			stat = 1
+		_info_panel.modulate = Color(1, 1, 1, alpha/255.0*2)
 	pass
 	
 func _on_body_entered(body):
@@ -21,16 +39,22 @@ func _on_body_entered(body):
 			var animation_player = $"../../../../Player/CamRoot2/ControllableCamera/GimbalH/GimbalV/Camera3D/AnimationPlayer"
 			var anim_name = "none"
 			var destination
+			#-----------------------------------#
 			if main_object == "info_turner_1":
 				destination = Vector3(4.436, 0.064, -10.56)
 				animation_player.play("animation_eks_1")
+			#-----------------------------------#
 			if main_object == "work_turner_1":
 				destination = Vector3(10.326, 0.064, 4.227)
 				animation_player.play("animation_work_1")
+			#-----------------------------------#
+			colorize = true
+			stat = 2
 			player.global_transform.origin = destination
 			animation_player.play(anim_name)
 
 func _on_body_exited(body):
 	local_locker = 0
 	print("выход из области.")
+
 
--- wood_turner_inside_scene/turner_init.gd
+++ wood_turner_inside_scene/turner_init.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
+@onready var _info_panel: Panel = $Player/interface/Panel
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SongScene.GLOBAL_OLD_SCENE = SongScene.GLOBAL_NOW_SCENE
 	SongScene.GLOBAL_NOW_SCENE = 3
+	_info_panel.modulate = Color(1, 1, 1, 0)
 	pass # Replace with function body.
